6eadb96d43041c5862bbe316b1c9a035c45e59a8
yu <g01965250@gmail.com>
2021-10-10 14:38:07 +0800

    line banner finish

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,5 +1,10 @@
 import LineLogo from './image/icon-title-pc.png';
 import LangIcon from './image/icon-lang-b.png';
+import SmLineLogo from './image/icon-line-w.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faApple ,faGooglePlay} from '@fortawesome/free-brands-svg-icons'
+import { faDesktop } from '@fortawesome/free-solid-svg-icons'
+
 
 
 
@@ -14,21 +19,41 @@ function Line() {
             <img src={LineLogo} alt="損壞"></img>
           </a>
         </div>
-        <div className='title'>
+        <div className='nav-title'>
           <a href='#'>Life on LINE</a>
           <a href='#'>通訊軟體</a>
           <a href='#'>服務總覽</a>
         </div>
         <div>
-          <div className='ball'>
-            <a href='#'>
+          <div className='ball-wrapper'>
+            <div className='ball'>
                 <img  src={LangIcon} alt="損壞"/>
               <div className='language'>TW-</div>
-            </a>
+            </div>
           </div>
         </div>
       </div>
     </header>
+    <main>
+      <div className='banner-background'>
+        <div className='banner-title-wrapper'>
+          <h2>Life on LINE</h2>
+          <h3>LINE始終陪伴在你身旁。</h3>
+        </div>
+        <div className='banner-icon-wrapper'>
+          <div className='download-icon-title'>
+            <img src={SmLineLogo} alt='損壞'/>
+            <p>下載</p>
+          </div>
+          <div className='download-wrapper'>
+            <div className='download-icon'><FontAwesomeIcon icon={faApple}/></div>
+            <div className='download-icon'><FontAwesomeIcon icon={faGooglePlay}/></div>
+            <div className='download-icon'><FontAwesomeIcon icon={faDesktop}/></div>
+          </div>
+        </div>
+      </div>
+    </main>
+    
     </>
   )
 
